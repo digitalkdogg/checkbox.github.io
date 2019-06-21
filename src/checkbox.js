d96e41e10f7c0f0d028a1e5e14425dd7bceec027
Kevin Bollman <kevin.bollman@walmart.com>
2019-06-21 08:02:25 -0500

    clean up js and wrapped in eventlistiner for dom ready

--- src/checkbox.js
+++ src/checkbox.js
@@ -1,53 +1,58 @@
-var checkbox = {
-  'checkboxes': {},
-  'utils': {
-    'get_class_list': function (ele) {
-      return ele.classList;
-    },
-    'check_for_check': function (ele, classstr) {
-      var classarray = ele.classList.value.split(' ');
-      if (classarray.indexOf(classstr)>=0) {
-        return true;
-      } else {
-        return false;
-      }
-    },//end checxk for check
-    'set_color_attr' : function (ele) {
-      var attr = ele.getAttribute('data-color')
-      if (attr != null) {
-        ele.setAttribute('style', 'color:'+ attr +';');
-      }
-    },//end set color attr
-    'set_style_attr' : function (ele) {
-      var attr = ele.getAttribute('data-style')
-      if (attr != null) {
-        if (attr.indexOf(';')>1) {
-          attr = attr.replace(';', '');
-        }
-        ele.setAttribute('style', attr+';');
-      }
-    }
-  }
-}
+"use strict";
+(function() {
+  document.addEventListener('DOMContentLoaded', function () {
 
-setTimeout(function () {
-  var checkboxes = document.querySelectorAll("[id^=checkbox]");
-  if(checkboxes.length > 0) {
-    for (var x = 0; x<checkboxes.length; x++) {
-      checkbox.utils.set_color_attr(checkboxes[x]);
-      checkbox.utils.set_style_attr(checkboxes[x]);
+    var checkbox = {
+      'checkboxes': {},
+      'utils': {
+          'get_class_list': function (ele) {
+              return ele.classList;
+            },
+            'check_for_check': function (ele, classstr) {
+                var classarray = ele.classList.value.split(' ');
+                if (classarray.indexOf(classstr)>=0) {
+                    return true;
+                } else {
+                    return false;
+                }
+              },//end checxk for check
+              'set_color_attr' : function (ele) {
+                  var attr = ele.getAttribute('data-color')
+                  if (attr != null) {
+                      ele.setAttribute('style', 'color:'+ attr +';');
+                  }
+              },//end set color attr
+              'set_style_attr' : function (ele) {
+                  var attr = ele.getAttribute('data-style')
+                  if (attr != null) {
+                      if (attr.indexOf(';')>1) {
+                          attr = attr.replace(';', '');
+                      }
+                      ele.setAttribute('style', attr+';');
+                  }
+              }//end set style attr
+          }//end utils
+      }//end checkbox
 
-      var index = 'checkbox_'+x;
-      checkbox.checkboxes[index] = checkboxes[x];
-      checkboxes[x].addEventListener("click", function() {
-        var element = this
-        if (checkbox.utils.check_for_check(this, 'checked') ===true) {
-          this.classList.remove("checked");
-        } else {
-          this.classList.add("checked");
-        }
-        checkbox.checkboxes[index] = this;
-      });
-    }//end for
-  }//end if
-}) //end settimeout
+      var checkboxes = document.querySelectorAll("[id^=checkbox]");
+
+      if(checkboxes.length > 0) {
+          for (var x = 0; x<checkboxes.length; x++) {
+              checkbox.utils.set_color_attr(checkboxes[x]);
+              checkbox.utils.set_style_attr(checkboxes[x]);
+
+              var index = 'checkbox_'+x;
+              checkbox.checkboxes[index] = checkboxes[x];
+              checkboxes[x].addEventListener("click", function() {
+              var element = this
+                if (checkbox.utils.check_for_check(this, 'checked') ===true) {
+                    this.classList.remove("checked");
+                } else {
+                    this.classList.add("checked");
+                }
+                checkbox.checkboxes[index] = this;
+              });
+          }//end for
+      }//end if
+  }, false); //end add listerner
+}()); //end anomynous function
